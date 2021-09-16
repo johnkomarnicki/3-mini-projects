3b76e81e326b9743e7f480e066a2277af4caed62
JohnRay1997 <johnkomarnickicontact@gmail.com>
2021-09-16 06:03:29 -0400

    article component

--- article-component/app.js
+++ article-component/app.js
@@ -1,3 +1,17 @@
 // store ".share" & ".share-img" in variables
+const share = document.querySelector(".share");
+const shareIcon = document.querySelector(".share-img");
 
-// listen for click on icon to show/hide share menu
+// listen for click on icon
+shareIcon.addEventListener("click", (e) => {
+  // Within listner
+  // show/hide correct share menu
+  // avoid shutting the share popup when clicking on it
+  if (e.target === shareIcon) {
+    if (window.innerWidth <= 750) {
+      share.classList.toggle("active-share-mobile");
+      return;
+    }
+    share.classList.toggle("active-share-desktop");
+  }
+});
