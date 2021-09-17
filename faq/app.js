c2c6a12801d09adb1e8316f8d2f6b6cf1ec6c309
JohnRay1997 <johnkomarnickicontact@gmail.com>
2021-09-17 10:51:33 -0400

    faq accordion card

--- faq/app.js
+++ faq/app.js
@@ -1,9 +1,30 @@
 // store ".accordion-q" & ".faq-card" in variables
+const questions = document.querySelectorAll(".accordion-q");
+const faqCard = document.querySelector(".faq-card");
 
 // listen for a click on any of the questions
+questions.forEach((question) => {
+  // Within listner
+  question.addEventListener("click", () => {
+    // Get height of answer
+    const height = question.nextElementSibling.scrollHeight;
 
-// Within listner
-// Get height of answer
-// Check if question has already been clicked
-// reset all questions/answers back to initial state
-// add active class to question selected & apply height
+    // Check if question has already been clicked close &&
+    // return out of function
+    if (question.parentElement.classList.contains("active")) {
+      question.parentElement.classList.remove("active");
+      question.nextElementSibling.style.maxHeight = "0px";
+      return;
+    }
+
+    // reset all questions/answers back to initial state
+    questions.forEach((q) => {
+      q.parentElement.classList.remove("active");
+      q.nextElementSibling.style.maxHeight = "0px";
+    });
+
+    // add active class to question selected & apply height
+    question.parentElement.classList.add("active");
+    question.nextElementSibling.style.maxHeight = `${height}px`;
+  });
+});
